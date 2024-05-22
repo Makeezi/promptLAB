2b8cbe53630827810dfc2a1ca6b41f09e9f74f4c
markus.heinonen <make.heinonen@outlook.com>
2024-05-23 01:28:22 +0300

    scrolling works, button styles

--- gui/lab.py
+++ gui/lab.py
@@ -3,7 +3,8 @@ import pip._vendor.requests as requests
 from flask_socketio import SocketIO, emit
 from PySide6.QtWidgets import QApplication, QMainWindow, QLineEdit, QVBoxLayout, QWidget,QGroupBox, QHBoxLayout, QTextEdit, QPushButton, QScrollArea, QSizePolicy, QFrame
 from PySide6.QtCore import Slot, Signal, QThread
-from PySide6.QtGui import QTextOption
+from PySide6.QtGui import QTextOption, QGuiApplication
+from PySide6.QtCore import Qt
 import sys
 from socketio import Client
 
@@ -89,37 +90,78 @@ class Conditioning(QHBoxLayout):
         # self.addWidget(self.negative_input)
         self.group_box.setLayout(self)
 
+buttonStyle = """
+    QPushButton {
+        background-color: #1B1A55;
+        border: 1px solid black;
+        color: #9290C3;
+        padding: 10px;
+        border-radius: 5px;
+    }
+    QPushButton:hover {
+        background-color: #535C91;
+    }
+    QPushButton:pressed {
+        background-color: #9290C3;
+        color: #1B1A55;
+    }
+"""
+
 
 # https://colorhunt.co/palette/070f2b1b1a55535c919290c3
 class MainWindow(QMainWindow):
     def __init__(self, sio_thread):
         super().__init__()
+
+        screen = QGuiApplication.screens()[0]
+        screen_size = screen.size()
+
+        # Set window size to be 75% of the screen size
+        self.resize(screen_size.width() * 0.75, screen_size.height() * 0.75)
+        # Set window size to be 75% of the screen size
+        
         self.sio_thread = sio_thread
         # Set window title
         self.setWindowTitle("Input App")
+
         self.setStyleSheet("background-color: #070F2B;")  # Black background
 
         self.layout = QVBoxLayout()
-        self.conditioning_layouts = []  # Initialize the list of row layouts
 
         self.addRow()
 
         # Create a button for adding new rows
         add_row_button = QPushButton("Add Row")
         add_row_button.clicked.connect(self.addRow)
-        self.layout.addWidget(add_row_button)
+        add_row_button.setStyleSheet(buttonStyle)
+        
+
+        # Create a scroll area and set its widget resizable property to True
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setFrameShape(QFrame.NoFrame)
+
+        # Create a widget for the scroll area and set its layout
+        scroll_widget = QWidget()
+        scroll_widget.setLayout(self.layout)
+        scroll_area.setWidget(scroll_widget)
+        
 
         # Set central widget
+        self.column = QVBoxLayout()
+        self.column.addWidget(scroll_area)
+        self.column.addWidget(add_row_button)
         central_widget = QWidget()
-        central_widget.setLayout(self.layout)
+        central_widget.setLayout(self.column)
         self.setCentralWidget(central_widget)
 
     def addRow(self):
         # Create a new QHBoxLayout for this row
         row_layout = QHBoxLayout()
-
+        row_layout.setSpacing(0)
         plus_button = QPushButton("+")
-        plus_button.clicked.connect(self.add_conditioning)
+        plus_button.clicked.connect(lambda: self.add_conditioning(row_layout))
+        plus_button.setStyleSheet(buttonStyle)
 
         # Create a scroll area and set its widget resizable property to True
         scroll_area = QScrollArea()
@@ -130,6 +172,8 @@ class MainWindow(QMainWindow):
         scroll_widget = QWidget()
         scroll_widget.setLayout(row_layout)
         scroll_area.setWidget(scroll_widget)
+        scroll_area.setMinimumHeight(300)
+        scroll_area.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 
         self.row = QHBoxLayout()
         self.row.addWidget(scroll_area)
@@ -137,14 +181,11 @@ class MainWindow(QMainWindow):
 
         self.layout.addLayout(self.row)
 
-        # Store the row layout so we can add Conditioning widgets to it later
-        self.conditioning_layouts.append(row_layout)
-
-    def add_conditioning(self):
+    def add_conditioning(self, row_layout):
         sharedCondition = SharedCondition(0, "pos", True)
         conditioning = Conditioning(self.sio_thread, sharedCondition)
         # Add the Conditioning widget to the last row's layout
-        self.conditioning_layouts[-1].addWidget(conditioning.group_box)
+        row_layout.addWidget(conditioning.group_box)
 
 def main():
     # Create the application
